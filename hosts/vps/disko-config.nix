da2883c12013e4dc0bbab75c80d6b8670a7e3027
XhuyZ <xhuyzdev@gmail.com>
2025-11-14 20:04:55 -0800

    Refactor disk configuration in disko-config.nix
    Updated disk configuration to include filesystem label for root.

--- hosts/vps/disko-config.nix
+++ hosts/vps/disko-config.nix
@@ -1,27 +1,24 @@
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/sda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; 
-              label = "disk-main-boot";
-            };
+    disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            size = "1M";
+            type = "EF02";
+          };
 
-            root = {
-              size = "100%";
-              label = "disk-main-root"; 
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = [ "noatime" "nodiratime" "discard" ];
-              };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              label = "nixos-root";   # filesystem label (CHUẨN)
+              mountpoint = "/";
+              mountOptions = [ "noatime" "nodiratime" "discard" ];
             };
           };
         };
