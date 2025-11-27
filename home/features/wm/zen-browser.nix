d1e1f1cb03ce164cd340ab0b91f0711f7f693508
xhuyz <xhuyzdev@gmail.com>
2025-11-28 02:36:40 +0700

    up

--- home/features/wm/zen-browser.nix
+++ home/features/wm/zen-browser.nix
@@ -55,6 +55,7 @@ in
               "wappalyzer@crunchlabz.com" = "wappalyzer";
               "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
               "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = "vimium-ff";
+              "{uBlock@raymondhill.net}" = "uBlock Origin";
             };
           };
       };
