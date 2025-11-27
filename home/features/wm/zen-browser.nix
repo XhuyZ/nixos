745ab87ca67e3059b6b7f214293884d9100af530
xhuyz <xhuyzdev@gmail.com>
2025-11-28 02:32:52 +0700

    up

--- home/features/wm/zen-browser.nix
+++ home/features/wm/zen-browser.nix
@@ -54,7 +54,7 @@ in
             ExtensionSettings = mkExtensionSettings {
               "wappalyzer@crunchlabz.com" = "wappalyzer";
               "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
-              "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = "vimium";
+              "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = "vimium-ff";
             };
           };
       };
