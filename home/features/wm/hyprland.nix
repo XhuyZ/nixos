3fa1cfd413c691526fb8e48aa75c03fdcf81a4aa
XhuyZ <xhuyzdev@gmail.com>
2026-01-01 10:23:56 +0700

    update

--- home/features/wm/hyprland.nix
+++ home/features/wm/hyprland.nix
@@ -12,9 +12,9 @@ in
   options.features.wm.hyprland.enable = mkEnableOption "Hyprland";
 
   config = mkIf cfg.enable {
-    # home.packages = with pkgs; [
-    #   firefox-devedition
-    # ];
+    home.packages = with pkgs; [
+      hyprland
+    ];
     programs.hyprland.enable = true;
   };
 }
