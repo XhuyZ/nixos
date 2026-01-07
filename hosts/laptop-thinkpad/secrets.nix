27c701f83cb5c93da6896d9071f4d7e9336bf4c6
XhuyZ <xhuyzdev@gmail.com>
2026-01-07 14:31:44 +0700

    update gnome

--- hosts/laptop-thinkpad/secrets.nix
+++ hosts/laptop-thinkpad/secrets.nix
@@ -1,13 +1,9 @@
 {
   age = {
     secrets = {
-      traefik = {
-        file = ../../secrets/traefik.age;
-        owner = "traefik";
-      };
-      m3tam3re-secrets = {
-        file = ../../secrets/m3tam3re-secrets.age;
-        owner = "m3tam3re";
+      thinkpad-ssh = {
+        file = ../../secrets/thinkpad.age;
+        owner = "xhuyz";
       };
     };
   };
