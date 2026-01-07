fa632589a6c05ede769718f69b1e7e143d89b7a8
XhuyZ <xhuyzdev@gmail.com>
2026-01-07 14:35:13 +0700

    gnome final

--- home/features/wm/gnome.nix
+++ home/features/wm/gnome.nix
@@ -21,37 +21,40 @@ in
     # GNOME configuration via dconf
     dconf.settings = {
       # Shell configuration
-      # "org/gnome/shell" = {
-      #   favorite-apps = [
-      #     "org.gnome.Nautilus.desktop"
-      #     "org.gnome.Console.desktop"
-      #   ];
-      disable-user-extensions = false;
-      enabled-extensions = [
-        "paperwm@paperwm.github.com"
-      ];
+      "org/gnome/shell" = {
+        # favorite-apps = [
+        #   "org.gnome.Nautilus.desktop"
+        #   "org.gnome.Console.desktop"
+        # ];
+        disable-user-extensions = false;
+        enabled-extensions = [
+          "paperwm@paperwm.github.com"
+        ];
+      };
+
+      # Touchpad
+      "org/gnome/desktop/peripherals/touchpad" = {
+        disable-while-typing = false;
+      };
+
+      # Window manager
+      "org/gnome/desktop/wm/preferences" = {
+        num-workspaces = 5;
+      };
+
+      # Workspace keybindings
+      "org/gnome/desktop/wm/keybindings" = {
+        # Workspace switching
+        switch-to-workspace-1 = [ "<Super>1" ];
+        switch-to-workspace-2 = [ "<Super>2" ];
+        switch-to-workspace-3 = [ "<Super>3" ];
+        switch-to-workspace-4 = [ "<Super>4" ];
+        switch-to-workspace-5 = [ "<Super>5" ];
+        # Application switching
+        switch-applications = [ "<Super><Tab>" ];
+        switch-applications-backward = [ "<Shift><Super><Tab>" ];
+      };
+
     };
-
-    # Touchpad
-    # "org/gnome/desktop/peripherals/touchpad" = {
-    #   disable-while-typing = false;
-    # };
-
-    # Window manager
-    # "org/gnome/desktop/wm/preferences" = {
-    #   num-workspaces = 5;
-    # };
-
-    # Workspace keybindings
-    # "org/gnome/desktop/wm/keybindings" = {
-    #   # Workspace switching
-    #   switch-to-workspace-1 = [ "<Super>1" ];
-    #   switch-to-workspace-2 = [ "<Super>2" ];
-    #   switch-to-workspace-3 = [ "<Super>3" ];
-    #   switch-to-workspace-4 = [ "<Super>4" ];
-    #   switch-to-workspace-5 = [ "<Super>5" ];
-    #   switch-applications = [ "<Super><Tab>" ];
-    #   switch-applications-backward = [ "<Shift><Super><Tab>" ];
-    # };
   };
 }
