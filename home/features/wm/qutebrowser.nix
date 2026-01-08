4b9651d83c74493f61b98b13fa67eda26bd4d4fc
XhuyZ <xhuyzdev@gmail.com>
2026-01-08 08:30:52 +0700

    qutebrowser homemanager

--- home/features/wm/qutebrowser.nix
+++ home/features/wm/qutebrowser.nix
@@ -15,5 +15,13 @@ in
     home.packages = with pkgs; [
       qutebrowser
     ];
+    programs.qutebrowser = {
+      searchEngines = {
+        w = "https://en.wikipedia.org/wiki/Special:Search?search={}&amp;go=Go&amp;ns0=1";
+        aw = "https://wiki.archlinux.org/?search={}";
+        nw = "https://wiki.nixos.org/index.php?search={}";
+        g = "https://www.google.com/search?hl=en&amp;q={}";
+      };
+    };
   };
 }
