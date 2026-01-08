0d6dea2730367bcc49d6dffbb3533457cd72984b
XhuyZ <xhuyzdev@gmail.com>
2026-01-08 08:34:57 +0700

    update qute

--- home/features/wm/qutebrowser.nix
+++ home/features/wm/qutebrowser.nix
@@ -12,15 +12,23 @@ in
   options.features.wm.qutebrowser.enable = mkEnableOption "Qutebrowser";
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      qutebrowser
-    ];
     programs.qutebrowser = {
+      enable = true;
       searchEngines = {
         w = "https://en.wikipedia.org/wiki/Special:Search?search={}&amp;go=Go&amp;ns0=1";
         aw = "https://wiki.archlinux.org/?search={}";
         nw = "https://wiki.nixos.org/index.php?search={}";
         g = "https://www.google.com/search?hl=en&amp;q={}";
+      };
+      settings = {
+        colors = {
+          hints = {
+            bg = "#000000";
+            fg = "#ffffff";
+          };
+          tabs.bar.bg = "#000000";
+        };
+        tabs.tabs_are_windows = true;
       };
     };
   };
