f8fb49277f391b9319ac380b8b6283f483c3fc68
XhuyZ <xhuyzdev@gmail.com>
2026-01-11 14:49:20 +0700

    add home data dir for webui

--- hosts/common/extraServices/ollama.nix
+++ hosts/common/extraServices/ollama.nix
@@ -17,6 +17,9 @@ in
       port = 11111;
       host = "0.0.0.0";
       openFirewall = true;
+      environment = {
+        DATA_DIR = "/home/xhuyz/ollama-data/webui";
+      };
     };
     services.ollama = {
       enable = true;
