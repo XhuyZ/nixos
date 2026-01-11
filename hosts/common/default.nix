cce0d6ddcfb8abed79288dea7f1836f981964217
XhuyZ <xhuyzdev@gmail.com>
2026-01-11 14:30:55 +0700

    edit mirror

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,6 +1,12 @@
 # Common configuration for all hosts
 
-{ lib, inputs, outputs, ... }: {
+{
+  lib,
+  inputs,
+  outputs,
+  ...
+}:
+{
   nixpkgs = {
     # You can add overlays here
     overlays = [
@@ -32,15 +38,24 @@
         "root"
         "xhuyz"
       ]; # Set users that are allowed to use the flake command
+      # Cache configuration - optimized for Asia
+      substituters = [
+        "https://mirror.sjtu.edu.cn/nix-channels/store" # Shanghai Jiao Tong University - best for Asia
+        "https://mirrors.ustc.edu.cn/nix-channels/store" # USTC backup mirror
+        "https://cache.nixos.org" # Official global cache
+        "https://nix-community.cachix.org" # Community packages
+        # "https://hyprland.cachix.org"
+        # "https://aseipp-nix-cache.global.ssl.fastly.net"
+      ];
     };
     gc = {
       automatic = true;
       options = "--delete-older-than 30d";
     };
     optimise.automatic = true;
-    registry = (lib.mapAttrs (_: flake: { inherit flake; }))
-      ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
+      (lib.filterAttrs (_: lib.isType "flake")) inputs
+    );
     nixPath = [ "/etc/nix/path" ];
   };
 }
-
