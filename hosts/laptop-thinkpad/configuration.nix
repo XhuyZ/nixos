c5de14368b71fc1c47f11e10dcc255d68596f77b
xhuyz <xhuyzdev@gmail.com>
2026-01-15 12:21:58 +0700

    comment gdm

--- hosts/laptop-thinkpad/configuration.nix
+++ hosts/laptop-thinkpad/configuration.nix
@@ -61,9 +61,9 @@
   networking.networkmanager.enable = true;
 
   ## --- GUI: GNOME Desktop --
-  services.displayManager.gdm.enable = true;
+  # services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.wayland = true;
+  # services.displayManager.gdm.wayland = true;
   services.xserver.xkb = {
     layout = "us";
     variant = "";
