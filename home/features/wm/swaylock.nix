ff7f00aa6e0ba2d13751e61881a3c7cecb10cb6a
xhuyz <xhuyzdev@gmail.com>
2026-01-15 22:44:10 +0700

    fix

--- home/features/wm/swaylock.nix
+++ home/features/wm/swaylock.nix
@@ -14,14 +14,14 @@ in
   config = mkIf cfg.enable {
     programs.swaylock = {
       enable = true;
-      settings = {
-        color = "808080";
-        font-size = 24;
-        indicator-idle-visible = false;
-        indicator-radius = 100;
-        line-color = "ffffff";
-        show-failed-attempts = true;
-      };
+      # settings = {
+      #   color = "808080";
+      #   font-size = 24;
+      #   indicator-idle-visible = false;
+      #   indicator-radius = 100;
+      #   line-color = "ffffff";
+      #   show-failed-attempts = true;
+      # };
     };
   };
 }
