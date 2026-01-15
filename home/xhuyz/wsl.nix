760f1a0b8f7bd2106e7c594017522adfa724221f
xhuyz <xhuyzdev@gmail.com>
2026-01-15 23:47:45 +0700

    fix

--- home/xhuyz/wsl.nix
+++ home/xhuyz/wsl.nix
@@ -1,17 +1,27 @@
-{ config, pkgs, lib, inputs, outputs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  outputs,
+  ...
+}:
 {
   imports = [
     ../common
-    ../features/programs
+    ../features/terminal
+    ../features/cli
     ../features/packages
-    ../features/wm
+    ../features/devkit
     ./home.nix
   ];
   features = {
-    programs = {
+    terminal = {
       nushell.enable = true;
       starship.enable = true;
       kitty.enable = true;
+    };
+    cli = {
       fastfetch.enable = true;
       gh.enable = true;
       cava.enable = true;
@@ -19,9 +29,15 @@
       jq.enable = true;
       lazydocker.enable = true;
       k9s.enable = true;
+      git.enable = true;
+      fcitx5.enable = true;
+      copyq.enable = true;
+    };
+    devkit = {
+      nodejs.enable = true;
+      dotnet_10.enable = false;
+      dotnet_9.enable = true;
+      dotnet_8.enable = false;
     };
   };
 }
-
-
-
