c520ed8a80db70c7779be4d1c0e7d8beba5a7957
xhuyz <xhuyzdev@gmail.com>
2026-01-15 12:37:35 +0700

    fix

--- hosts/laptop-thinkpad/services/display-manager.nix
+++ hosts/laptop-thinkpad/services/display-manager.nix
@@ -2,11 +2,15 @@
 {
   services.displayManager = {
     enable = true;
+    defaultSession = "mango";
     sddm = {
       enable = true;
       wayland = {
         enable = true;
       };
+      extraPackages = [
+        pkgs.sddm-astronaut
+      ];
       theme = "sddm-astronaut-theme";
     };
   };
