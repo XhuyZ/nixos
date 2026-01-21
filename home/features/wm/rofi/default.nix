7b0873468164dc7ee583b91110e723513432c89a
xhuyz <xhuyzdev@gmail.com>
2026-01-22 03:46:08 +0700

    u rofi

--- home/features/wm/rofi/default.nix
+++ home/features/wm/rofi/default.nix
@@ -5,9 +5,7 @@
   ...
 }:
 with lib;
-
 let
-  inherit (config.lib.formats.rasi) mkLiteral;
   cfg = config.features.wm.rofi;
 in
 {
@@ -27,33 +25,6 @@ in
         pkgs.rofi-emoji
         pkgs.rofi-bluetooth
       ];
-
-      theme = {
-        "*" = {
-          background = mkLiteral "rgba(0,43,55,1)";
-          foreground = mkLiteral "rgba(129,147,150,1)";
-          border-color = mkLiteral "@foreground";
-          spacing = 2;
-        };
-
-        "#window" = {
-          background-color = mkLiteral "@background";
-          border = 1;
-          padding = 5;
-        };
-
-        "#listview" = {
-          spacing = 2;
-          scrollbar = true;
-        };
-
-        "#textbox-prompt-colon" = {
-          expand = false;
-          str = ":";
-          margin = mkLiteral "0px 0.3em 0em 0em";
-          text-color = mkLiteral "@foreground";
-        };
-      };
     };
   };
 }
