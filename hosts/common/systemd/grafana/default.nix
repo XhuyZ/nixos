ab635c5cd2b49c66402a566ed23b7e655ee05a7f
XhuyZ <xhuyzdev@gmail.com>
2026-01-26 02:17:00 +0700

    update grafana and prometheus

--- hosts/common/systemd/grafana/default.nix
+++ hosts/common/systemd/grafana/default.nix
@@ -14,6 +14,21 @@ in
 {
   options.systemd.grafana.enable = mkEnableOption "enable grafana";
   config = mkIf cfg.enable {
-    services.grafana.enable = true;
+    services.grafana = {
+      enable = true;
+      settings = {
+        server = {
+          http_addr = "127.0.0.1";
+          http_port = 3000;
+          enforce_domain = true;
+          enable_gzip = true;
+          # domain = "grafana.your.domain";
+          # domain = "your.domain";
+          # root_url = "https://your.domain/grafana/";
+          # serve_from_sub_path = true;
+        };
+      };
+
+    };
   };
 }
