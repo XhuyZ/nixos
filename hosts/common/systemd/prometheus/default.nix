8bba311af524cbe449dbb5f24f948145f563af3c
XhuyZ <xhuyzdev@gmail.com>
2026-01-25 23:41:37 +0700

    update prometheus

--- hosts/common/systemd/prometheus/default.nix
+++ hosts/common/systemd/prometheus/default.nix
@@ -14,6 +14,38 @@ in
 {
   options.systemd.prometheus.enable = mkEnableOption "enable prometheus";
   config = mkIf cfg.enable {
-    services.prometheus.enable = true;
+    services.prometheus = {
+      enable = true;
+      port = 9000;
+      globalConfig.scrape_interval = "10s"; # "1m"
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [
+            {
+              targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+            }
+          ];
+        }
+      ];
+      # For the list of available collectors, run, depending on your install:
+      # - Flake-based: nix run nixpkgs#prometheus-node-exporter -- --help
+      # - Classic: nix-shell -p prometheus-node-exporter --run "node_exporter --help"
+      enabledCollectors = [
+        "ethtool"
+        "softirqs"
+        "systemd"
+        "tcpstat"
+        "wifi"
+      ];
+      # You can pass extra options to the exporter using `extraFlags`, e.g.
+      # to configure collectors or disable those enabled by default.
+      # Enabling a collector is also possible using "--collector.[name]",
+      # but is otherwise equivalent to using `enabledCollectors` above.
+      extraFlags = [
+        "--collector.ntp.protocol-version=4"
+        "--no-collector.mdadm"
+      ];
+    };
   };
 }
