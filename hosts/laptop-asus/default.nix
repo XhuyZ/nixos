ec2bdee39e595a3ffc31e926c5acefbb62b53a9f
xhuyz <xhuyzdev@gmail.com>
2026-02-02 03:12:55 +0700

    fix

--- hosts/laptop-asus/default.nix
+++ hosts/laptop-asus/default.nix
@@ -8,18 +8,27 @@
   ];
 
   systemd = {
+    # LLM
     ollama.enable = true;
-    qemu.enable = true;
+    # Container Management
     podman.enable = true;
+    # Databases
     postgresql.enable = true;
+    # Monitoring
     grafana.enable = true;
     prometheus.enable = true;
+    # Automation workflows
     n8n.enable = true;
+    # VMs + System container
     incus.enable = true;
-    traefik.enable = false;
+    qemu.enable = true;
+    # VPN
     wireguard.enable = true;
+    # Reverse Proxy
     nginx.enable = true;
-    pihole.enable = true;
+    traefik.enable = false;
+    # DNS server
+    pihole.enable = false;
     coredns.enable = true;
   };
 }
