3bfdcecf0ecfdd4699fbf9da87f610accd953f78
xhuyz <xhuyzdev@gmail.com>
2026-02-02 03:44:07 +0700

    fix

--- home/features/gui/teams-for-linux/default.nix
+++ home/features/gui/teams-for-linux/default.nix
@@ -6,14 +6,14 @@
 }:
 with lib;
 let
-  cfg = config.features.gui.rider;
+  cfg = config.features.gui.teams-for-linux;
 in
 {
-  options.features.gui.rider.enable = mkEnableOption "Rider";
+  options.features.gui.teams-for-linux.enable = mkEnableOption "Rider";
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-			teams-for-linux
+      teams-for-linux
     ];
   };
 }
