24d03d8810eb9edbd767efccc45ab36d49303c9b
xhuyz <xhuyzdev@gmail.com>
2026-03-16 11:45:34 +0700

    update

--- hosts/orion/secrets.nix
+++ hosts/orion/secrets.nix
@@ -1,16 +1,16 @@
-{ config, ... }:
-{
-  age = {
-    identityPaths = [ "/home/xhuyz/.ssh/id_ed25519" ];
-    secrets = {
-      grafana-username = {
-        file = ../../secrets/grafana-username.age;
-        owner = "xhuyz";
-      };
-      grafana-password = {
-        file = ../../secrets/grafana-password.age;
-        owner = "xhuyz";
-      };
-    };
-  };
-}
+# { config, ... }:
+# {
+#   age = {
+#     identityPaths = [ "/home/xhuyz/.ssh/id_ed25519" ];
+#     secrets = {
+#       grafana-username = {
+#         file = ../../secrets/grafana-username.age;
+#         owner = "xhuyz";
+#       };
+#       grafana-password = {
+#         file = ../../secrets/grafana-password.age;
+#         owner = "xhuyz";
+#       };
+#     };
+#   };
+# }
