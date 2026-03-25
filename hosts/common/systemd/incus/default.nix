5c42a9f54c92b7ac0072b35c492564c8737ae0aa
xhuyz <xhuyzdev@gmail.com>
2026-03-26 00:20:46 +0700

    fix

--- hosts/common/systemd/incus/default.nix
+++ hosts/common/systemd/incus/default.nix
@@ -14,56 +14,57 @@ in
   config = mkIf cfg.enable {
     virtualisation.incus = {
       enable = true;
-      # preseed = {
-      #   networks = [
-      #     {
-      #       config = {
-      #         "ipv4.address" = "10.0.100.1/24";
-      #         "ipv4.nat" = "true";
-      #       };
-      #       name = "incusbr0";
-      #       type = "bridge";
-      #     }
-      #   ];
-      #   profiles = [
-      #     {
-      #       devices = {
-      #         eth0 = {
-      #           name = "eth0";
-      #           network = "incusbr0";
-      #           type = "nic";
-      #         };
-      #         root = {
-      #           path = "/";
-      #           pool = "default";
-      #           size = "35GiB";
-      #           type = "disk";
-      #         };
-      #       };
-      #       name = "default";
-      #     }
-      #   ];
-      #   storage_pools = [
-      #     {
-      #       config = {
-      #         source = "/var/lib/incus/storage-pools/default";
-      #       };
-      #       driver = "dir";
-      #       name = "default";
-      #     }
-      #   ];
-      # };
+      preseed = {
+        networks = [
+          {
+            config = {
+              "ipv4.address" = "10.0.100.1/24";
+              "ipv4.nat" = "true";
+              "dns.mode" = "none";
+            };
+            name = "incusbr0";
+            type = "bridge";
+          }
+        ];
+        profiles = [
+          {
+            devices = {
+              eth0 = {
+                name = "eth0";
+                network = "incusbr0";
+                type = "nic";
+              };
+              root = {
+                path = "/";
+                pool = "default";
+                size = "35GiB";
+                type = "disk";
+              };
+            };
+            name = "default";
+          }
+        ];
+        storage_pools = [
+          {
+            config = {
+              source = "/var/lib/incus/storage-pools/default";
+            };
+            driver = "dir";
+            name = "default";
+          }
+        ];
+      };
     };
     networking.nftables.enable = true;
-    # networking.firewall.interfaces.incusbr0 = {
-    #   allowedTCPPorts = [
-    #     53
-    #     67
-    #   ];
-    #   allowedUDPPorts = [
-    #     53
-    #     67
-    #   ];
-    # };
+    networking.firewall.interfaces.incusbr0 = {
+      allowedTCPPorts = [
+        53
+        67
+      ];
+      allowedUDPPorts = [
+        53
+        67
+      ];
+    };
   };
 }
