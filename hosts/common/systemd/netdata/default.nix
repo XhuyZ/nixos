ef25b5b3e78ce01f9ad722a3b82409c8c8a4b653
xhuyz <xhuyzdev@gmail.com>
2026-03-26 22:12:43 +0700

    update netdata

--- hosts/common/systemd/netdata/default.nix
+++ hosts/common/systemd/netdata/default.nix
@@ -32,16 +32,6 @@ in
           mode = "full"; # enable WebUI
         };
       };
-
-      # Optional: disable unused plugins to reduce warnings/log spam
-      plugins = {
-        freeipmi = {
-          enable = false;
-        }; # no IPMI sensors
-        alarm-notify = {
-          enable = false;
-        }; # no notification script
-      };
     };
 
     ## --- Firewall for Netdata ---
