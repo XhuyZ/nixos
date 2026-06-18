a241a79e3569c474cbeb56d8a6a76b73536d53dc
xhuyz <xhuyzdev@gmail.com>
2026-06-19 00:06:36 +0700

    last

--- hosts/common/systemd/initrd-tailscale/default.nix
+++ hosts/common/systemd/initrd-tailscale/default.nix
@@ -5,43 +5,58 @@
   ...
 }:
 
+with lib;
+
 let
   cfg = config.systemd.initrd-tailscale;
 in
 {
-  boot.initrd.systemd.packages = [
-    cfg.package
-  ];
+  options.systemd.initrd-tailscale = {
+    enable = mkEnableOption "Enable Tailscale in initrd";
 
-  boot.initrd.systemd.initrdBin = [
-    pkgs.iproute2
-    pkgs.iptables
-    cfg.package
-  ];
-
-  boot.initrd.availableKernelModules = [
-    "tun"
-    "nft_chain_nat"
-  ];
-
-  boot.initrd.systemd.services.tailscaled = {
-    description = "Tailscale in initrd";
-
-    wantedBy = [
-      "initrd.target"
-    ];
-
-    after = [
-      "network-online.target"
-    ];
-
-    serviceConfig = {
-      # ExecStart = "${cfg.package}/bin/tailscaled";
-      ExecStart = "${cfg.package}/bin/tailscaled --state=/srv/tailscale/tailscaled.state";
-      Environment = [
-        "PORT=41641"
-        "TS_DEBUG_FIREWALL_MODE=nftables"
-      ];
+    package = mkOption {
+      type = types.package;
+      default = pkgs.tailscale;
     };
+  };
+
+  config = mkIf cfg.enable {
+
+    boot.initrd.systemd.packages = [
+      cfg.package
+    ];
+
+    boot.initrd.systemd.initrdBin = [
+      pkgs.iproute2
+      pkgs.iptables
+      cfg.package
+    ];
+
+    boot.initrd.availableKernelModules = [
+      "tun"
+      "nft_chain_nat"
+    ];
+
+    boot.initrd.systemd.services.tailscaled = {
+      description = "Tailscale in initrd";
+
+      wantedBy = [
+        "initrd.target"
+      ];
+
+      after = [
+        "network-online.target"
+      ];
+
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/tailscaled --state=/srv/tailscale/tailscaled.state";
+
+        Environment = [
+          "PORT=41641"
+          "TS_DEBUG_FIREWALL_MODE=nftables"
+        ];
+      };
+    };
+
   };
 }
