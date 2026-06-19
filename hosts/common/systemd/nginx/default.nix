1a659774830b2066cefe8f632f22ee9abab355c3
xhuyz <xhuyzdev@gmail.com>
2026-06-19 23:29:45 +0700

    u

--- hosts/common/systemd/nginx/default.nix
+++ hosts/common/systemd/nginx/default.nix
@@ -16,6 +16,17 @@ in
       enable = true;
 
       virtualHosts = {
+        "git.xhuyz.me" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3001";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto https;
+            '';
+          };
+        };
         "grafana.lab" = {
           locations."/" = {
             proxyPass = "http://127.0.0.1:3000";
