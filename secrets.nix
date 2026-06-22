c16def430b4e4e02185e4e31553c041d05e21047
xhuyz <xhuyzdev@gmail.com>
2026-06-22 22:17:17 +0700

    u

--- secrets.nix
+++ secrets.nix
@@ -7,7 +7,7 @@ let
   orion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNPjUQbyiFjT6KwxiZ8LT++7jgLLRD9iPOga9FhKJyq";
 
   # USERS change from where using the agenix remote to
-  xhuyz = laptop-asus;
+  xhuyz = laptop-thinkpad;
   # xhuyz = laptop-asus;
   users = [ xhuyz ];
 
