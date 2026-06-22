8e3fe23e74f3e4b4cbbc521b8bfd445358aa5125
xhuyz <xhuyzdev@gmail.com>
2026-06-22 22:31:41 +0700

    u

--- hosts/orion/secrets.nix
+++ hosts/orion/secrets.nix
@@ -1,19 +1,10 @@
 {
   age = {
-    age.identityPaths = [
+    identityPaths = [
       "/etc/ssh/ssh_host_ed25519_key"
     ];
+
     secrets = {
-      grafana-username = {
-        file = ../../secrets/grafana-username.age;
-        owner = "root";
-      };
-
-      grafana-password = {
-        file = ../../secrets/grafana-password.age;
-        owner = "root";
-      };
-
       forgejo-runner-token = {
         file = ../../secrets/forgejo-runner-token.age;
         owner = "root";
--- secrets.nix
+++ secrets.nix
@@ -1,26 +1,16 @@
 let
   # SYSTEMS
-  laptop-asus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgm3sX4DwDHljKd+YEMAzf4oq4cn0Y7CAVrgD2BkX+7";
   laptop-thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0yDi7KEb3BJ+K4WXscZDV/iuGGnoMNhClzEEz/iDtT";
-  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyHuLITpI+M45ZZem33wDusY2X988mBoWpD1HDeZNRJ";
-  vps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjbaZU8KsJn70OssH7QGQ6ISOJJfykMjMLgUZzHzG1v";
   orion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNPjUQbyiFjT6KwxiZ8LT++7jgLLRD9iPOga9FhKJyq";
 
   # USERS change from where using the agenix remote to
-  xhuyz = laptop-thinkpad;
-  # xhuyz = laptop-asus;
-  users = [ xhuyz ];
+  users = [ laptop-thinkpad ];
 
   systems = [
-    laptop-asus
     laptop-thinkpad
-    wsl
-    vps
     orion
   ];
 in
 {
-  "secrets/grafana-username.age".publicKeys = systems ++ users;
-  "secrets/grafana-password.age".publicKeys = systems ++ users;
   "secrets/forgejo-runner-token.age".publicKeys = systems ++ users;
 }
