a522a8613a2db789e7de23eaa1d05b92442c9ac0
xhuyz <xhuyzdev@gmail.com>
2026-06-22 22:57:44 +0700

    u

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 let
   # SYSTEMS
-  laptop-thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0yDi7KEb3BJ+K4WXscZDV/iuGGnoMNhClzEEz/iDtT";
+  laptop-thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLhI8f9r78Jebj3OB9IWlPTDlygHXAznFqxGzULuqdq deploy-rs";
   orion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNPjUQbyiFjT6KwxiZ8LT++7jgLLRD9iPOga9FhKJyq";
 
   # USERS change from where using the agenix remote to
