6b86179ee7fcec0310ffa4254566186c9094d401
XhuyZ <xhuyzdev@gmail.com>
2026-07-13 08:53:02 +0700

    remove desktopu

--- home/xhuyz/wsl.nix
+++ home/xhuyz/wsl.nix
@@ -14,12 +14,8 @@
     ../features/packages
     ../features/devkit
     ./home.nix
-    ./features/desktop
   ];
   features = {
-    desktop = {
-      gnome.enable = false;
-    };
     terminal = {
       nushell.enable = true;
       starship.enable = true;
