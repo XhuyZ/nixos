21cd2bcee8e4f3fc763d15d22a5e2ab04fa4e1e8
XhuyZ <xhuyzdev@gmail.com>
2026-08-16 20:45:36 +0700

    u

--- home/xhuyz/home.nix
+++ home/xhuyz/home.nix
@@ -28,6 +28,7 @@
     enable = true;
     targets.gtk.enable = false;
     targets.qt.enable = false;
+    targets.gnome.enable = false;
     image = ../../assets/laptop-thinkpad.png;
     polarity = "dark";
   };
