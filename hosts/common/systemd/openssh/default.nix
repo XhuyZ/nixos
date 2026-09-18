500fbd182c04f9e9b5df791fe044093cfd25db0f
XhuyZ <xhuyzdev@gmail.com>
2026-09-19 02:05:17 +0700

    u

--- hosts/common/systemd/openssh/default.nix
+++ hosts/common/systemd/openssh/default.nix
@@ -22,7 +22,7 @@ in
 
       settings = {
         PermitRootLogin = "no";
-        PasswordAuthentication = false;
+        PasswordAuthentication = yes;
         PubkeyAuthentication = true;
       };
     };
