2ff1c388197fa094537c31d2460d06c0280d94ca
XhuyZ <xhuyzdev@gmail.com>
2026-09-22 21:42:12 +0700

    test wireguard

--- hosts/common/systemd/wireguard/default.nix
+++ hosts/common/systemd/wireguard/default.nix
@@ -1,19 +1,64 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
+
 with lib;
+
 let
   cfg = config.systemd.wireguard;
 in
 {
-  options.systemd.wireguard.enable = mkEnableOption "enable wireguard";
+  options.systemd.wireguard.enable = mkEnableOption "enable WireGuard";
 
   config = mkIf cfg.enable {
-    networking.wireguard = {
+    networking.useNetworkd = true;
+
+    systemd.network = {
       enable = true;
+
+      networks."50-wg0" = {
+        matchConfig.Name = "wg0";
+
+        address = [
+          "10.10.99.2/32"
+        ];
+      };
+
+      netdevs."50-wg0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "wg0";
+        };
+
+        wireguardConfig = {
+          ListenPort = 13231;
+
+          # sau này đưa vào agenix
+          PrivateKeyFile = "/path/to/private-key";
+
+          RouteTable = "main";
+        };
+
+        wireguardPeers = [
+          {
+            PublicKey = "ROUTEROS_PUBLIC_KEY";
+
+            AllowedIPs = [
+              "10.10.10.0/24"
+              "10.10.20.0/24"
+              "10.10.30.0/24"
+              "10.10.40.0/24"
+              "10.10.50.0/24"
+            ];
+
+            Endpoint = "YOUR_HOME_PUBLIC_IP:13231";
+
+            PersistentKeepalive = 25;
+          }
+        ];
+      };
     };
   };
 }
